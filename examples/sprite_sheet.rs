a1a110deccbfcd21f3e5efb0892ce7cb3f8afded
Verte <vertesian@protonmail.com>
2024-12-16 14:29:57 +0530

    update sprite and sprite_sheet examples

--- examples/sprite_sheet.rs
+++ examples/sprite_sheet.rs
@@ -1,94 +1,67 @@
 use bevy::prelude::*;
 use bevy_sprite3d::prelude::*;
 
-#[derive(States, Hash, Clone, PartialEq, Eq, Debug, Default)]
-enum GameState { #[default] Loading, Ready }
-
-#[derive(Resource, Default)]
-struct ImageAssets {
-    image: Handle<Image>,               // the `image` field here is only used to query the load state, lots of the
-    layout: Handle<TextureAtlasLayout>, // code in this file disappears if something like bevy_asset_loader is used.
-}
-
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
 
 fn main() {
-
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugins(Sprite3dPlugin)
-        .init_state::<GameState>()
-
-        // initially load assets
-        .add_systems(Startup, |asset_server:         Res<AssetServer>,
-                               mut assets:           ResMut<ImageAssets>,
-                               mut texture_atlases:  ResMut<Assets<TextureAtlasLayout>>| {
-
-            assets.image = asset_server.load("gabe-idle-run.png");
-            assets.layout = texture_atlases.add(
-                TextureAtlasLayout::from_grid(UVec2::new(24, 24), 7, 1, None, None)
-            );
-        })
-
-        // run `setup` every frame while loading. Once it detects the right
-        // conditions it'll switch to the next state.
-        .add_systems(Update, setup.run_if(in_state(GameState::Loading)))
-
-        // every frame, animate the sprite
-        .add_systems(Update, animate_sprite.run_if(in_state(GameState::Ready)))
-
-        .insert_resource(ImageAssets::default())
+        .add_systems(Startup, setup)
+        .add_systems(Update, animate_sprite)
         .run();
 
 }
 
 fn setup(
-    asset_server      : Res<AssetServer>,
-    assets            : Res<ImageAssets>,
-    mut commands      : Commands,
-    mut next_state    : ResMut<NextState<GameState>>,
-    mut sprite_params : Sprite3dParams
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+    mut billboards: ResMut<Assets<Billboard>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut texture_atlases: ResMut<Assets<TextureAtlasLayout>>,
 ) {
+    let image = asset_server.load("gabe-idle-run.png");
+    let layout = texture_atlases.add(
+        TextureAtlasLayout::from_grid(UVec2::new(24, 24), 7, 1, None, None)
+    );
 
-    // poll every frame to check if assets are loaded. Once they are, we can proceed with setup.
-    if !asset_server.get_load_state(assets.image.id()).is_some_and(|s| s.is_loaded()) { return; }
-    next_state.set(GameState::Ready);
-
-    // -----------------------------------------------------------------------
-
-    commands.spawn(Camera3d::default()).insert(Transform::from_xyz(0., 0., 5.));
-
-    // -------------------- Spawn a 3D atlas sprite --------------------------
-
+    let billboard =
+        Billboard::with_texture_atlas(image.clone(), layout.clone(), 32., None, false);
     let texture_atlas = TextureAtlas {
-        layout: assets.layout.clone(),
+        layout,
         index: 3,
     };
 
-    commands.spawn(Sprite3dBuilder {
-            image: assets.image.clone(),
-            pixels_per_metre: 32.,
-            alpha_mode: AlphaMode::Blend,
-            unlit: true,
-            // pivot: Some(Vec2::new(0.5, 0.5)),
-            ..default()
-    }.bundle_with_atlas(&mut sprite_params, texture_atlas))
-    .insert(AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)));
+    let material = StandardMaterial {
+        alpha_mode: AlphaMode::Blend,
+        unlit: true,
+        ..default()
+    };
 
-    // -----------------------------------------------------------------------
+    // spawn the camera
+    commands.spawn(Camera3d::default()).insert(Transform::from_xyz(0., 0., 5.));
+
+    // spawn an animated 3D sprite
+    commands.spawn((
+        Sprite3d::from(texture_atlas),
+        Sprite3dBillboard::new(billboards.add(billboard)),
+        MeshMaterial3d(materials.add(material)),
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+    ));
 }
 
 
 fn animate_sprite(
     time: Res<Time>,
+    atlas_layouts: Res<Assets<TextureAtlasLayout>>,
     mut query: Query<(&mut AnimationTimer, &mut Sprite3d)>,
 ) {
     for (mut timer, mut sprite_3d) in query.iter_mut() {
         timer.tick(time.delta());
         if timer.just_finished() {
-            let length = sprite_3d.texture_atlas_keys.as_ref().unwrap().len();
             let atlas = sprite_3d.texture_atlas.as_mut().unwrap();
+            let length = atlas_layouts.get(&atlas.layout).unwrap().textures.len();
             atlas.index = (atlas.index + 1) % length;
         }
     }
